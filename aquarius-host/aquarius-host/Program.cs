a243f1013967a305d2a9a0e55e24f89e798893d3
Shinsuke Akabane <bane.shinsuke@gmail.com>
2013-01-25 00:08:35 +0900

    added

--- aquarius-host/aquarius-host/Program.cs
+++ aquarius-host/aquarius-host/Program.cs
@@ -15,7 +15,7 @@ namespace aquarius_host
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
