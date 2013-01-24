1d7843938f32f40e09ac3bb9bb99524b22620add
Shinsuke Akabane <bane.shinsuke@gmail.com>
2013-01-25 00:12:06 +0900

    add
    Signed-off-by: Shinsuke Akabane <bane.shinsuke@gmail.com>

---
+++ aquarius-host/aquarius-host/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace aquarius_host
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(85, 137);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(105, 35);
+            this.connectBtn.TabIndex = 0;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(85, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "COM0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(85, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Reset Power";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(83, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 372);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.connectBtn);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ aquarius-host/aquarius-host/MainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace aquarius_host
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+
+        }
+
+        enum STATUS
+        {
+            NOT_CONNECT,
+            CONNECTED,
+            ERROR,
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // 0時判定
+            
+            SendChar('n');
+            
+        }
+
+        private void SendChar(char c)
+        {
+            char[] buff = new char[1];
+
+
+            buff[0] = c;
+            // Send the one character buffer.
+            serialPort1.Write(buff, 0, 1);
+
+            this.label1.Text = "'" + c + "' was sent!!";
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.label1.Text = STATUS.NOT_CONNECT.ToString();
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                serialPort1.PortName = this.textBox1.Text;
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+                this.label1.Text = STATUS.CONNECTED.ToString();
+
+                this.button1.Enabled = true;
+
+                this.timer1.Enabled = true;
+            }
+            catch
+            {
+                this.label1.Text = STATUS.ERROR.ToString();
+                //this.label1.Text += "\n";
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendChar('n');
+        }
+    }
+}
