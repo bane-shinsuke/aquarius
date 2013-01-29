c9624ad2492cef2ada0c1f05d8b74690a7934fab
Shinsuke Akabane <bane.shinsuke@gmail.com>
2013-01-29 10:44:42 +0900

    aquarius-web作成。

---
+++ aquarius-web/WEB-INF/src/net/bane/aquarius/rest/HelloResource.java
@@ -0,0 +1,27 @@
+package net.bane.aquarius.rest;
+
+import java.util.Calendar;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//The Java class will be hosted at the URI path "/helloworld"
+@Path("/power")
+public class HelloResource {
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String turnPower( @FormParam("value") String value) {
+    	Calendar now = Calendar.getInstance(); //インスタンス化
+
+    	System.out.println(value);
+        int h = now.get(now.HOUR_OF_DAY);//時を取得
+        int m = now.get(now.MINUTE);     //分を取得
+        int s = now.get(now.SECOND);      //秒を取得
+        System.out.println(h+"時"+m+"分"+s+"秒");
+        return "Success!! " + h+":"+m+":"+s;
+    }
+}
